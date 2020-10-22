a378bd3ce62639dcdac89bd60719ba1098395609
Matteo <Rascioni>
2020-10-22 17:50:15 +0200

    100%

--- js/main.js
+++ js/main.js
@@ -1,25 +1,61 @@
-// PARI E DISPARI 
-var numUtente = parseInt( prompt('Inserisci un numero' ) );
+// // PARI E DISPARI 
+// var sceltaUtente = prompt('Scegli pari o dispari');
 
-somma = pariDispari(numUtente) + getRandomNumber();
+// var numUtente = parseInt( prompt('Inserisci un numero' ) );
 
-if (somma == 'pari') {
-    console.log('Il numero passato è pari');
+// var numPc = getRandomNumber(1, 5);
+// console.log(numPc);
+
+// var somma = numUtente + numPc;
+
+// var pariDisparo = pariDispari(somma);
+
+// console.log(somma);
+
+// if (pariDisparo == sceltaUtente) {
+//     console.log('hai vinto');
+// } else {
+//     console.log('hai perso');
+// }
+
+// // FUNZIONE PARI O DISPARI
+// function pariDispari(numero) {
+//     if (numero % 2 == 0 ) {
+//         return 'pari'
+//     } else {
+//       return 'dispari'
+//     }
+// }
+
+// // FUNZIONE NUMERI RANDOM 
+// function getRandomNumber(min, max) {
+//     var random = Math.floor(Math.random() * (max - min + 1) + min);
+//     return random;
+// }
+
+
+// PALINDROMA
+var parolaUtente = prompt('Inserisci la parola');
+console.log('Parola: ', parolaUtente);
+
+// INVOCAZIONE 
+parolaGirata = wordReverse(parolaUtente);
+console.log('Parola al contrario: ', parolaGirata);
+
+if (parolaGirata == parolaUtente) {
+    console.log('la parola è palindroma');
 } else {
-    console.log('Il numero passato è dispari');
+    console.log('La parola non è palindroma');
 }
 
-// FUNZIONE PARI O DISPARI
-function pariDispari(numero) {
-    if (numero % 2 == 0 ) {
-        return 'pari'
+// FUNCTION WORD REVERSE
+function wordReverse(word) {
+    
+    var reverse = '';
+
+    for (var i = word.length - 1; i >= 0; i-- ) {
+        reverse += word[i];
     }
-    return 'dispari'
-}
 
-
-// FUNZIONE NUMERI RANDOM 
-function getRandomNumber(min, max) {
-    var random = Math.floor(Math.random() * (max - min + 1) + min);
-    return (random);
+    return reverse; 
 }
